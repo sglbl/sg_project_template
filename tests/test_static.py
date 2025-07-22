8ff612ff23d353e7079f89edadd94283488cf417
Süleyman <silo1881@gmail.com>
2025-07-22 16:00:55 +0200

    minor

--- tests/test_static.py
+++ tests/test_static.py
@@ -1,11 +1,10 @@
-import pyright
-
 """
 pytest -s --tb=no tests/test_static.py
 """
+import pyright
+
 
 def test_with_pyright():
-    # Run pyright
+    """ Run pyright to check for static type errors in the codebase. """
     response = pyright.cli.run()
     assert response.returncode == 0, f"Pyright return code was {response.returncode}, expected 0. Please check the output for static errors."
-    
